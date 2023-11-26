c9ff3c9cb7fae4c6df051f1f52fa0d803021032a
Zahoris.Leonardo <zahoris.leonardo@one-beyond.com>
2023-11-26 21:06:06 +0100

    adding more code

--- server/app.py
+++ server/app.py
@@ -9,11 +9,13 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello World From Flask!'
+    return "Hello World From Flask!"
+
+
 app.config.from_object(ApplicationConfig)
-app.config['SESSION_TYPE'] =  "redis"
+app.config["SESSION_TYPE"] = "redis"
 
 bcrypt = Bcrypt(app)
 server_session = Session(app)
@@ -23,126 +25,159 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
-
-#Get current user
-@app.route("/current")
-def get_current_user():
-    user_id = session.get("user_id")
-
-
-    if not user_id:
-        return jsonify({"error": "Unauthorized"}), 401
     
-    user = User.query.filter_by(id=user_id).first()
-    return jsonify({
-        "id": user.id, 
-        "email": user.email
-        
-    }) 
+##********************USERS*********************#
 
-#Get current student
-
-@app.route("/currentStudent")
-def get_current_student():
-    student_id = session.get("student_id")
- 
-    if not student_id:
-        return jsonify({"error": "Unauthorized"}), 401
-    student = Student.query.filter_by(id=student_id).first()
-    return jsonify({
-        "id": student.id,
-        "email": student.email
-       
-    })
-
-# get all users
-@app.route('/users', methods=['GET'])
-def get_users():  
-  return jsonify([
-        {
-        "id": user.id,  
-        "email": user.email,
-      
-        } for user in User.query.all()
-     ])
-
-#updating a user
-@app.route('/users/<int:id>', methods=['PUT'])
-def update_user(id):
-    
-     if not id:
-        return jsonify({"error": "Unauthorized"}), 401
-    
-     user = User.query.filter_by(id=id).first()
-     
-     if user:
-       user.email = request.json["email"]
-       db.session.commit()
-     return jsonify({"message": "user updated correctly"}), 200
-    
-#add a new student
-@app.route("/registerstudent", methods=["POST"])
-def register_student():
-    firstname = request.json["firstname"]
-    lastname =  request.json["lastname"]
-    email = request.json["email"]
-    age = request.json["age"]
-
-    student_exists = Student.query.filter_by(email=email).first() is not None
-
-    if student_exists:
-        return jsonify({"error": "Student already exists"}), 409
-
-    new_student= Student(firstname=firstname, lastname=lastname,email=email, age=age)
-    db.session.add(new_student)
-    db.session.commit()
-    
-    session["student_id"] = new_student.id
-
-    return jsonify({
-        "id": new_student.id,
-        "firstname": new_student.firstname,
-        "lastname": new_student.lastname,
-        "email": new_student.email,
-        "age": new_student.age,
-    })
-
-# get all students
-@app.route('/students', methods=['GET'])
-def get_students():
-  
- return jsonify([
-        {
-        "id": student.id,
-        "firstname": student.firstname,
-        "lastname": student.lastname,
-        "email": student.email,
-        "age": student.age,
-        } for student in Student.query.all()
-     ])
-
+# Register a user
 @app.route("/register", methods=["POST"])
 def register_user():
     email = request.json["email"]
     password = request.json["password"]
+    try:
 
-    user_exists = User.query.filter_by(email=email).first() is not None
+        user_exists = User.query.filter_by(email=email).first() is not None
 
-    if user_exists:
-        return jsonify({"error": "User already exists"}), 409
+        if user_exists:
+            return jsonify({"error": "User already exists"}), 409
 
-    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
-    new_user = User(email=email, password=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
+        hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+        new_user = User(email=email, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        session["user_id"] = new_user.id
+        return jsonify({'message': 'user created',"id": new_user.id, "email": new_user.email})
+    except:
+        return jsonify({'error': 'error creating user'}), 500
+
+# Get current user
+@app.route("/current")
+def get_current_user():
+    user_id = session.get("user_id")
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({"id": user.id, "email": user.email})
+
+# get a user by id
+@app.route('/users/<int:id>', methods=['GET'])
+def get_user(id):
+  try:
+    user = User.query.filter_by(id=id).first()
+    if user:
+      return jsonify({"id": user.id, "email": user.email}), 200
+    return jsonify({'message': 'user not found'}), 404
+  except:
+    return jsonify({'message': 'error getting user'}), 500
+
+# get all users
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify(
+        [
+            {
+                "id": user.id,
+                "email": user.email,
+            }
+            for user in User.query.all()
+        ]
+    )
+
+# updating a user by id
+@app.route("/users/<int:id>", methods=["PUT"])
+def update_user(id):
+
+    if not id:
+        return jsonify({"error": "Unauthorized"}), 401
     
-    session["user_id"] = new_user.id
+    try:
+        user = User.query.filter_by(id=id).first()
 
-    return jsonify({
-        "id": new_user.id,
-        "email": new_user.email
-    })
+        if user:
+            user.email = request.json["email"]
+            db.session.commit()
+            return jsonify({'message': 'user updated',"id": user.id, "email": user.email}), 200
+    except:
+        return jsonify({'message': 'error getting user'}), 500
 
+
+# add a new student
+
+# delete a user
+@app.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+  try:
+    user = User.query.filter_by(id=id).first()
+    if user:
+      db.session.delete(user)
+      db.session.commit()      
+      return jsonify({'message': 'user deleted'}), 200
+    return jsonify({'message': 'user not found'}), 404
+  except:
+    return jsonify({'message': 'error getting user'}), 500
+
+
+#********************STUDENTS************************#
+
+@app.route("/registerstudent", methods=["POST"])
+def register_student():
+
+    try:
+        firstname = request.json["firstname"]
+        lastname = request.json["lastname"]
+        email = request.json["email"]
+        age = request.json["age"]
+
+        student_exists = Student.query.filter_by(email=email).first() is not None
+
+    except student_exists:
+        return jsonify({"error": "Student already exists"}), 409
+
+        new_student = Student(
+            firstname=firstname, lastname=lastname, email=email, age=age
+        )
+        db.session.add(new_student)
+        db.session.commit()
+
+        session["student_id"] = new_student.id
+
+        return jsonify(
+            {
+                "id": new_student.id,
+                "firstname": new_student.firstname,
+                "lastname": new_student.lastname,
+                "email": new_student.email,
+                "age": new_student.age,
+            }
+        )
+
+# Get current student
+@app.route("/currentStudent")
+def get_current_student():
+    student_id = session.get("student_id")
+
+    if not student_id:
+        return jsonify({"error": "Unauthorized"}), 401
+    student = Student.query.filter_by(id=student_id).first()
+    return jsonify({"id": student.id, "email": student.email})
+
+# get all students
+@app.route("/students", methods=["GET"])
+def get_students():
+
+    return jsonify(
+        [
+            {
+                "id": student.id,
+                "firstname": student.firstname,
+                "lastname": student.lastname,
+                "email": student.email,
+                "age": student.age,
+            }
+            for student in Student.query.all()])
+
+# Login
 @app.route("/login", methods=["POST"])
 def login_user():
     email = request.json["email"]
@@ -155,18 +190,17 @@ def login_user():
 
     if not bcrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Unauthorized"}), 401
-    
+
     session["user_id"] = user.id
 
-    return jsonify({
-        "id": user.id,
-        "email": user.email
-    })
+    return jsonify({"id": user.id, "email": user.email})
 
+# Logout
 @app.route("/logout", methods=["POST"])
 def logout_user():
     session.pop("user_id")
     return "200"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
